e6cdb5596ced387ff72d5f3c0380dc80d71cc8ff
Fajuu <36969029+Fajuu@users.noreply.github.com>
2019-04-15 17:01:41 +0200

    Delete less

--- extend.php
+++ extend.php
@@ -18,11 +18,9 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
+        ->js(__DIR__.'/js/dist/forum.js'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/less/admin.less'),
+        ->js(__DIR__.'/js/dist/admin.js'),
     new Extend\Locales(__DIR__ . '/locale'),
 
     function (Dispatcher $events) {
